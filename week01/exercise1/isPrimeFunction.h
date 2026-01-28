e84639c098908f5f8ac31530f86c97b1d8ca1084
Maxwell Fetterman <maxwell.fetterman@uconn.edu>
2026-01-28 14:03:50 +0000

    Finished assignments (exceptions handled)

--- week01/exercise1/isPrimeFunction.h
+++ week01/exercise1/isPrimeFunction.h
@@ -1,10 +1,15 @@
 #ifndef __ISPRIMEFUNCTION_H
 #define __ISPRIMEFUNCTION_H
+#include <cmath>
+
 
 bool isPrimeInteger(int pTest) {
     bool isPrime = true;
+    if (pTest == 2) return isPrime; //edge case 2
+    if (pTest < 1) return false; //edge case 1 or lower
+    
 
-    for (int i = 2; i < pTest; i++) {
+    for (int i = 2; i < static_cast<int>(floor(sqrt(pTest))); i++) {
         if (pTest % i == 0) {
             isPrime = false;
             break;
